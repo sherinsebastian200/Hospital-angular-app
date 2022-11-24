b750a967d215eee828b6fba89b62dbfe2b1f5e88
sherin200 <sherinsebastian21>
2022-11-24 11:58:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HospitalEntryComponent } from './hospital-entry/hospital-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
+import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HospitalEntryComponent,
-    PatientSearchComponent
+    PatientSearchComponent,
+    PatientDeleteComponent
   ],
   imports: [
     BrowserModule,
