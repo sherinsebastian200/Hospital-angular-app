92e3ba30d08078983762cf7cad46854a20b373ab
sherin200 <sherinsebastian21>
2022-11-24 12:57:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +6,45 @@ import { AppComponent } from './app.component';
 import { HospitalEntryComponent } from './hospital-entry/hospital-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
+import { RouterModule } from '@angular/router';
+import { PatientViewComponent } from './patient-view/patient-view.component';
+const myRoute:Routes=[
+  {
+    path:"",
+    Component:HospitalEntryComponent
+
+  }
+
+  {
+    path:"search",
+    Component:PatientSearchComponent
+  }
+
+  {
+    path:"delete",
+    Component:PatientDeleteComponent
+  
+  }
+  {
+    path:"view"
+    Component:PatientViewComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HospitalEntryComponent,
     PatientSearchComponent,
-    PatientDeleteComponent
+    PatientDeleteComponent,
+    PatientViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    RouterModule.forRoot(myRoute)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
